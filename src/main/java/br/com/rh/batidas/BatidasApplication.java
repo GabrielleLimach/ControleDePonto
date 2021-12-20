cb31a584e6bda01f21f2c1a265ea545309312efa
Gabrielle Limach <gabrielle.lmm@gmail.com>
2021-12-19 21:02:16 -0300

    M - iniciando projeto de batidas de pontos

---
+++ src/main/java/br/com/rh/batidas/BatidasApplication.java
@@ -0,0 +1,13 @@
+package br.com.rh.batidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatidasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rh/batidas/BatidasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rh.batidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
